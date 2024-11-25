2b3a13174d3c853c3fec0fbd10ebd9ac340868cc
duckwang900 <aarnwang900@gmail.com>
2024-11-25 18:46:14 -0500

    Aligned text boxes in sign up page

--- authorize/forms.py
+++ authorize/forms.py
@@ -5,7 +5,10 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}), required=True)
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}), label="Password")
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}), label="Confirm Password")
 
     class Meta:
         model = User
