61bdba7b0b85a143e2fe4eee9dd1ca3014b6eeaa
Eldridge <eldridgesurianto1@gmail.com>
2024-11-28 18:29:57 -0500

    changed home.html to index.html

--- Rewindify_App/urls.py
+++ Rewindify_App/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("authorize/", include("authorize.urls")),
     path("authorize/", include("django.contrib.auth.urls")),
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("", TemplateView.as_view(template_name="index.html"), name="home"),
 ]
